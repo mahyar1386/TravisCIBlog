0e76a74c8388a20619b8e807bfaf2522967d069f
mahyar <mahyar1386@gmail.com>
2019-01-30 13:06:03 +0330

    initial commit

---
+++ Jazzy Documentation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Jazzy Documentation
+//
+//  Created by Mahyar on 1/27/19.
+//  Copyright © 2019 Mahyar. All rights reserved.
+//
+
+import UIKit
+
+/// Main View Controller
+class ViewController: UIViewController {
+    
+    /// view did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
